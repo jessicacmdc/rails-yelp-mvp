2d4a9287fb7bd6a3ac39176cafe191e057849dbe
jessica mota <jessicam.arq.urb@gmail.com>
2025-08-13 13:06:46 +0200

    model and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# db/seeds.rb
+# 1. Clean the database 🗑️
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+puts "Creating restaurants..."
+Restaurant.create!(name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese")
+puts "Created Dishoom"
+Restaurant.create!(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian")
+puts "Created Pizza East"
+Restaurant.create!(name: "Epicure", address: "75008 Paris", category: "french")
+puts "Created Epicure"
+Restaurant.create!(name: "Kobe", address: "Ruiterij 1 Maastricht", category: "japanese")
+puts "Created Kobe"
+Restaurant.create!(name: "Witloof", address: "Sint Bernardusstraat 12, 6211 HL Maastricht", category: "belgian")
+puts "Created Witloof"
+
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."
